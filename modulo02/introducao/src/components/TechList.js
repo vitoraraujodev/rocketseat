5266dd5661ced3aa82424e9d6047df76358f12c8
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-11-09 19:02:31 -0300

    Propriedades do React

--- modulo02/introducao/src/components/TechList.js
+++ modulo02/introducao/src/components/TechList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import TechItem from './TechItem';
+
 class TechList extends Component {
   state={
     newTech: '',
@@ -24,19 +26,20 @@ class TechList extends Component {
   }
 
   handleDelete = (tech) => {
-    this.setState({ techs: this.state.techs.filter( t => t !== tech) })
+    this.setState({ techs: this.state.techs.filter( t => t !== tech ) })
   }
 
   render() {
     return(
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => (
-            <li key={tech}>
-              {tech}
-              <button onClick={() => this.handleDelete(tech)} type="button">Remover</button>
-            </li>
-          ))}
+          {this.state.techs.map(tech =>
+            <TechItem 
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          )}
         </ul>
         <input type="text"
           onChange={this.handleInputChange}
