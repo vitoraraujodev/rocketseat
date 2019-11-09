3e2bcd7a4a964d5881dc5975460e3becffbf25cc
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-11-09 14:56:31 -0300

    Importando imagens

--- modulo02/introducao/src/App.js
+++ modulo02/introducao/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import lolcoffee from './assets/lol-coffee.jpeg';
+
 function App() {
-  return <h1>Hello Rocketseat</h1>
+  return <img width="200" src={lolcoffee} />
 }
 
 export default App;
--- modulo02/introducao/webpack.config.js
+++ modulo02/introducao/webpack.config.js
@@ -24,6 +24,12 @@ module.exports = {
           { loader: 'css-loader' },
         ]
       }
+      , {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: [
+          { loader: 'file-loader' },
+        ]
+      }
     ]
   }
 };
