2dbebccdf62c676f0342d2be608970ec38068aa3
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-11-11 23:23:55 -0300

    PostList e PostItem

--- modulo02/desafio/src/App.js
+++ modulo02/desafio/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+
 import Header from './components/Header'
+import PostList from './components/PostList'
 
 import './App.css';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <PostList />
+    </>
   );
 }
 
